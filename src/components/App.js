64e94b9479e600e82f0ee8f48d778c4d4a4e5308
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-05-14 13:38:25 +0200

    Add redux

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,19 @@ import About from './About';
 import NotFound from './NotFound';
 import Posts from '../containers/Posts';
 
-export default () => {
-  return (
-    <div className="App">
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </div>
-      <hr/>
-      <Switch>
-        <Route exact path="/" component={Posts}/>
-        <Route exact path="/about" component={About}/>
-        <Route component={NotFound}/>
-      </Switch>
+export default () => (
+  <div className="App">
+    <div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+      </ul>
     </div>
-  );
-};
+    <hr/>
+    <Switch>
+      <Route exact path="/" component={Posts}/>
+      <Route exact path="/about" component={About}/>
+      <Route component={NotFound}/>
+    </Switch>
+  </div>
+);
