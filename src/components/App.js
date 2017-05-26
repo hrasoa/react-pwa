75cb2a7dd82e0209b7596f311c295a743d2b3125
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-05-26 22:55:48 +0200

    Update metas

--- src/components/App.js
+++ src/components/App.js
@@ -11,9 +11,6 @@ import '../manifest.json';
 export default withRouter(({ route }) => (
   <div className="App">
     <Helmet>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta name="theme-color" content="#FFFFFF" />
       <title>My App</title>
       <link rel="canonical" href="http://mysite.com/example" />
     </Helmet>
