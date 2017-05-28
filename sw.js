4c03c900d09fa1332fcc41fd66af9705ee571d0d
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-05-28 03:44:38 +0200

    Add service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
 (function(global) {
   importScripts('sw-toolbox.js');
 
+  var CACHE_NAME = 'my-pwa-cache-v1';
+
   var urlsToCache = [
     '/',
     '/style.css',
@@ -10,10 +12,12 @@
     '/bundle.js.gz'
   ];
 
-  global.toolbox.cache.name = 'my-pwa-cache-v1';
+  global.toolbox.cache.name = CACHE_NAME;
   global.toolbox.precache(urlsToCache);
   global.toolbox.options.debug = true;
   global.toolbox.router.default = global.toolbox.networkFirst;
+
+  global.toolbox.router.get(/(\.js|\.css|\.gz)$/, toolbox.cacheFirst);
 
   global.addEventListener('install',
     function(event) { return event.waitUntil(global.skipWaiting()); });
