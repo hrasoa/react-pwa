8e38ab98131c1570821faa9614b381672db97959
Haja Rasoahaingo <hrasoahaingo@purch.com>
2017-05-31 17:00:14 +0200

    Add sw-cache

--- precache.js
+++ precache.js
@@ -6,24 +6,23 @@ import pkg from './package.json';
 const publicDir = webpackConfig.output.path;
 
 swPrecache.write(`${publicDir}/sw.js`, {
-    cacheId: pkg.name,
-    dontCacheBustUrlsMatching: /./,
-    dynamicUrlToDependencies: {
-        '/': [resolve(__dirname, 'views', 'index.ejs')]
-    },
-    staticFileGlobs: [
-        publicDir + '/bundle.*.{js,css}',
-        publicDir + '/vendor.*.{js,css}'
-    ],
-    navigateFallback: '/',
-    navigateFallbackWhitelist: [
-        /^\/posts\//,
-        /^\/about/
-    ],
-    stripPrefix: publicDir,
-    runtimeCaching: [{
-        urlPattern: /api/,
-        handler: 'networkFirst'
-    }]
+  cacheId: pkg.name,
+  dontCacheBustUrlsMatching: /./,
+  dynamicUrlToDependencies: {
+    '/': [resolve(__dirname, 'views', 'index.ejs')]
+  },
+  staticFileGlobs: [
+    publicDir + '/bundle.*.{js,css}',
+    publicDir + '/vendor.*.{js,css}'
+  ],
+  navigateFallback: '/',
+  navigateFallbackWhitelist: [
+    /^\/posts\//,
+    /^\/about/
+  ],
+  stripPrefix: publicDir,
+  runtimeCaching: [{
+    urlPattern: /api/,
+    handler: 'networkFirst'
+  }]
 });
-
