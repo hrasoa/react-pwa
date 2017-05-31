990ff45e8d9368d07b567e217366d1cad357aeb4
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-05-31 23:26:37 +0200

    Add tests

---
+++ __tests__/actions/posts.test.js
@@ -0,0 +1,11 @@
+import * as actionsById from '../../src/actions/posts/byId';
+
+describe('actions by Id', () => {
+  it('should invalidate a post', () => {
+    const expectedAction = {
+      type: actionsById.INVALIDATE_SINGLE_POST,
+      postId: 1
+    };
+    expect(actionsById.invalidateSinglePost(1)).toEqual(expectedAction);
+  })
+});
