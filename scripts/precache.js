dd44dc91e10e3172414f2eac919e9f6b48f5b3a3
Haja Rasoahaingo <hrasoahaingo@purch.com>
2017-06-02 18:50:33 +0200

    Update precache

--- scripts/precache.js
+++ scripts/precache.js
@@ -15,7 +15,8 @@ swPrecache.generate({
   },
   navigateFallback: '/',
   staticFileGlobs: [
-    `${publicDir}/{bundle,vendor}.*.{js,css}`
+    `${publicDir}/{bundle,vendor}.*.{js,css,gz}`,
+    `${publicDir}/manifest.json`
   ],
   stripPrefix: publicDir,
   runtimeCaching: [{
