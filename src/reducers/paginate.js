771596d15416b270b9cc4cfec4bee0a502eb91cd
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-06-25 18:21:14 +0200

    Update reducer

--- src/reducers/paginate.js
+++ src/reducers/paginate.js
@@ -3,7 +3,11 @@ import union from 'lodash.union';
 
 // Creates a reducer managing pagination, given the action types to handle,
 // and a function telling how to extract the key from an action.
-export default function paginate({ types, mapActionToKey, mapActionResults = action => action }) {
+export default function paginate({
+  types,
+  mapActionToKey,
+  mapActionResults = action => action
+}) {
   if (!Array.isArray(types) || types.length !== 3) {
     throw new Error('Expected types to be an array of three elements.');
   }
