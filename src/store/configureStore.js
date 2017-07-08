515ecdfa53ffd980da6c87789a30c494b3fd7798
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-07-08 14:17:52 +0200

    Update HMR

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,6 +6,7 @@ import {
 } from 'redux';
 import createSagaMiddleware, { END } from 'redux-saga';
 import rootReducer from '../reducers/index';
+import SagaManager from '../sagas/sagaManager';
 
 export default function configureStore(initialState) {
   const composeEnhancers = (typeof window !== 'undefined' &&
@@ -20,14 +21,23 @@ export default function configureStore(initialState) {
     ))
   );
 
+  // run sagas
+  SagaManager.startSagas(sagaMiddleware);
+
   if (module.hot) {
     module.hot.accept('../reducers/index', () => {
       const nextRootReducer = require('../reducers/index').default;
       store.replaceReducer(nextRootReducer);
     });
+
+    module.hot.accept('../sagas/sagaManager', () => {
+      SagaManager.cancelSagas(store);
+      require('../sagas/sagaManager').default.startSagas(sagaMiddleware);
+    });
   }
 
   store.runSaga = sagaMiddleware.run;
   store.close = () => store.dispatch(END);
+
   return store;
 }
