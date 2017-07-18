b3e2d5d7f6e778aa1729a239c39f875120dbafe7
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-07-19 00:11:20 +0200

    Update api to use async

--- server/api/index.js
+++ server/api/index.js
@@ -6,58 +6,56 @@ import axios from 'axios';
 const router = express.Router();
 
 
-const getPaginated = (url, params = {}) => {
+async function getPaginated(url, params = {}) {
   const perPage = parseInt(params.query && params.query._perPage || 10, 10);
   const page = parseInt(params.query && params.query._page || 1, 10);
   const start = (page - 1) * perPage;
-  return axios.get(url)
-    .then(response => {
-      const data = response.data.slice(start, start + perPage);
-      if (params.query && params.query.fl) {
-        return data.map(item => params.query.fl.split(',').reduce((acc, field) => {
-          acc[field] = item[field];
-          return acc;
-        }, {}));
-      }
-      return data;
-    });
-};
+  const response = await axios.get(url);
+  const data = response.data.slice(start, start + perPage);
+  if (params.query && params.query.fl) {
+    return data.map(item => params.query.fl.split(',').reduce((acc, field) => {
+      acc[field] = item[field];
+      return acc;
+    }, {}));
+  }
+  return data;
+}
 
 
 const getPosts = getPaginated.bind(null, 'https://jsonplaceholder.typicode.com/posts');
 const getPictures = getPaginated.bind(null, 'https://unsplash.it/list');
 
 
-router.get('/posts', (req, res) => {
-  getPosts(url.parse(req.url, true)).then(data => {
-    res.send(data);
-  });
+router.get('/posts', async (req, res) => {
+  const data = await getPosts(url.parse(req.url, true));
+  res.send(data);
 });
 
 
-router.get('/posts/:id', (req, res) => {
-  axios.get(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`)
-    .then(response => {
-      res.send(response.data);
-    });
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${req.params.id}`)
+    res.send(response.data);
+  } catch(e) {
+    console.log(e);
+  }
 });
 
 
-router.get('/pictures', (req, res) => {
-  getPictures(url.parse(req.url, true)).then(data => {
-    res.send(data);
-  });
+router.get('/pictures', async (req, res) => {
+  const data = await getPictures(url.parse(req.url, true));
+  res.send(data);
 });
 
 
-router.get('/home', (req, res) => {
+router.get('/home', async (req, res) => {
   const posts = getPosts({ query: { fl: 'id,title' } });
   const pictures = getPictures({ query: { fl: 'id,filename' } });
-  Promise.all([ posts, pictures ]).then(results => {
-    res.send({
-      latestPosts: results[0],
-      latestPictures: results[1]
-    })
+  const latestPosts = await posts;
+  const latestPictures = await pictures;
+  res.send({
+    latestPosts,
+    latestPictures
   });
 });
 
