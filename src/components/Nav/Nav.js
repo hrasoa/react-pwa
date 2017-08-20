cbb45741103194c71cf0ef0237c81a9c92890e0c
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-08-20 15:58:16 +0200

    Update nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { getIsConnected } from '../../selectors/index';
 
 const links = [
   { title: 'Home', to: '/' },
   { title: 'About', to: '/about' },
-  { title: 'Login', to: '/login' },
   { title: 'Private', to: '/private' }
 ];
 
-export default () => (
+const Nav = ({ isConnected }) => (
   <nav className="c-nav">
     <ul>
       {links.map(link => (
@@ -22,6 +24,26 @@ export default () => (
           </Link>
         </li>
       ))}
+      {!isConnected &&
+        (<li>
+          <Link
+            className="c-nav__link"
+            title="Login"
+            to="/login"
+          >
+            Login
+          </Link>
+        </li>)}
     </ul>
   </nav>
 );
+
+Nav.propTypes = {
+  isConnected: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = state => ({
+  isConnected: getIsConnected(state)
+});
+
+export default connect(mapStateToProps)(Nav);
