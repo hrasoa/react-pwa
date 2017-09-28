4520afb960fda61df3831cac3299475c5ad58278
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-09-28 13:29:24 +0200

    Update App

--- src/components/App.js
+++ src/components/App.js
@@ -9,30 +9,28 @@ import PostPage from '../containers/AsyncPostPage/index';
 import LoginPage from '../containers/AsyncLoginPage/index';
 import PrivateRoute from '../containers/PrivateRoute';
 import Profile from '../containers/AsyncProfilePage/index';
-import Bundle from './AsyncBundle/index';
 import About from './AsyncAbout/index';
+import Bundle from './AsyncBundle/index';
 import Private from './AsyncPrivate/index';
 import Nav from './Nav';
 import NotFound from './AsyncNotFound/index';
 
-export default () => (
-  <div>
-    <Helmet>
-      <title>My App</title>
-      <link rel="canonical" href="http://example.com" />
-    </Helmet>
-    <Bundle />
-    <Nav />
-    <div className="o-container">
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/about" exact component={About} />
-        <Route path="/login" exact component={LoginPage} />
-        <Route path="/posts/:id" exact component={PostPage} />
-        <PrivateRoute path="/profile" exact component={Profile} />
-        <PrivateRoute path="/private" exact component={Private} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
+export default () => [
+  <Helmet key="1">
+    <title>My App</title>
+    <link rel="canonical" href="http://example.com" />
+  </Helmet>,
+  <Bundle key="2" />,
+  <Nav key="3" />,
+  <div key="4" className="o-container">
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/about" exact component={About} />
+      <Route path="/login" exact component={LoginPage} />
+      <Route path="/posts/:id" exact component={PostPage} />
+      <PrivateRoute path="/profile" exact component={Profile} />
+      <PrivateRoute path="/private" exact component={Private} />
+      <Route component={NotFound} />
+    </Switch>
   </div>
-);
+];
