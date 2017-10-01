a503dedb91bd74ff75817f4637bf8ac3a5a70f2d
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-10-01 14:38:16 +0200

    Update server: remove process.env read

--- server/app/prod.js
+++ server/app/prod.js
@@ -11,6 +11,11 @@ const app = express();
 const outputPath = webpackCommonConfig.paths.output;
 const outputServerPath = webpackCommonConfig.paths.outputServer;
 
+const serverRenderer = require(path.join(outputServerPath, 'prod.render.js')).default;
+const clientStats = require(path.join(outputPath, 'stats.json'));
+const bundleManifest = require(path.join(outputPath, 'bundle.json'));
+const mainCss = path.join(outputPath, bundleManifest['main.css']);
+
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(favicon(webpackCommonConfig.paths.favicon));
@@ -26,10 +31,6 @@ app.listen(3000, () => {
   console.log("\x1b[35m", 'BUILD COMPLETE -- Listening @ :3000');
 });
 
-const serverRenderer = require(path.join(outputServerPath, 'prod.render.js')).default;
-const clientStats = require(path.join(outputPath, 'stats.json'));
-const bundleManifest = require(path.join(outputPath, 'bundle.json'));
-const mainCss = path.join(outputPath, bundleManifest['main.css']);
 fs.readFile(mainCss, 'utf8', (err, data) => {
   if (err) throw err;
   app.use(serverRenderer({ clientStats, options: {
