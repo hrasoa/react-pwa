2b9750ed655a0deba7b832961e8b7bce847969d7
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-10-23 08:34:05 +0200

    Update models

--- server/api/models/index.js
+++ server/api/models/index.js
@@ -2,7 +2,7 @@ module.exports = (sequelize) => {
   const User = require('./user')(sequelize);
   const Post = require('./post')(sequelize);
   User.hasMany(Post, { foreignKey: 'user_id' });
-  Post.hasOne(User, { foreignKey: 'user_id' });
+  Post.belongsTo(User, { foreignKey: 'user_id' });
   return {
     User,
     Post
