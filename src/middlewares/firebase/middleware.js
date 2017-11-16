88ad1ec6a770decd7a51f1ffe3f935a3dc1648b4
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-11-16 21:28:29 +0100

    Update firebase middleware

--- src/middlewares/firebase/middleware.js
+++ src/middlewares/firebase/middleware.js
@@ -1,8 +1,8 @@
 import Firebase from './Firebase';
 import Auth from './Auth';
 
-export default function firebaseMiddleware(firebaseApp) {
-  const FirebaseApp = new Firebase(firebaseApp, [
+export default function firebaseMiddleware(app) {
+  const FirebaseApp = new Firebase(app, [
     new Auth()
   ]);
   return store => next => action => FirebaseApp.handleAction(action, next);
