3ad85b841398be6dd3924c62ba62de8cc3b521cc
Haja Rasoahaingo <haja.rasoah@gmail.com>
2017-11-16 23:54:05 +0100

    Update firebase middleware

--- src/middlewares/firebase/actionHandler.js
+++ src/middlewares/firebase/actionHandler.js
@@ -1,11 +1,9 @@
 export default (firebaseApp, handlers = []) => {
   const firebase = firebaseApp;
-
   const actions = handlers.reduce((acc, handler) => {
     const handlerActions = handler.mapActionsToApi();
     return { ...acc, ...handlerActions };
   }, {});
-
   return (action, next) => {
     const { type } = action;
     return actions[type] ? actions[type]({ firebase, action, next }) : next(action);
